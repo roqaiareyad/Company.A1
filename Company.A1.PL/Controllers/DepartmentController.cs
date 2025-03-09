87ce037664903acf1b072b06c1b4839257247206
roqaiareyad <141576333+roqaiareyad@users.noreply.github.com>
2025-03-09 03:33:42 +0200

    Create Create (Action - view)

--- Company.A1.PL/Controllers/DepartmentController.cs
+++ Company.A1.PL/Controllers/DepartmentController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Company.A1.BLL.Repositories;
 using Company.A1.BLL.Interfaces;
+using Company.A1.PL.Dtos;
+using Microsoft.Net.Http.Headers;
+using Company.A1.DAL.Models;
 namespace Company.A1.PL.Controllers
 {
     //MVC Controller
@@ -25,6 +28,33 @@ namespace Company.A1.PL.Controllers
 
 
             return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+        
+            if (ModelState.IsValid) //Server Side Validation
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreatedDate = model.CreatedDate
+                };
+         
+                var count = _departmentRepository.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+            return View();
         }
     }
 }
